0157a61f4e5a41699f780374c499b2207a0d2c71
josecuevaslanda <33970466+josecuevaslanda@users.noreply.github.com>
2019-11-20 12:28:07 -0800

    SQL Query

---
+++ SQLQuery.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE GYMDB
+USE GYMDB
+
+CREATE TABLE TipoUsuario
+(
+	id INT IDENTITY,
+	nombre VARCHAR(20),
+	tipousuario CHAR,
+	CONSTRAINT tipousuario_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE Usuario
+(
+	id INT IDENTITY,
+	nombre VARCHAR(20),
+	apaterno VARCHAR(20),
+	amaterno VARCHAR(20),
+	pin VARCHAR(20),
+	imgpath VARCHAR(250),
+	correo VARCHAR(150),
+	tipousuario_id INT,
+	CONSTRAINT usuario_pkey PRIMARY KEY (id),
+	CONSTRAINT tipousuario_fkey FOREIGN KEY (tipousuario_id) REFERENCES TipoUsuario(id)
+)
+
+CREATE TABLE Cliente
+(
+	usuario_id INT,
+	fnacimiento DATE,
+	peso DECIMAL,
+	estatura INT,
+	CONSTRAINT cliente_pkey PRIMARY KEY (usuario_id),
+	CONSTRAINT usuario_fkey FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
+)
+
+CREATE TABLE Ejercicio
+(
+	id INT IDENTITY,
+	nombre VARCHAR(30),
+	descripcion TEXT,
+	CONSTRAINT ejercicio_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE Rutina
+(
+	id INT IDENTITY,
+	nombre VARCHAR(30),
+	dia VARCHAR(15),
+	repeticiones INT,
+	peso DECIMAL,
+	ejercicio_id INT,
+	usuario_id INT,
+	CONSTRAINT rutina_pkey PRIMARY KEY (id),
+	CONSTRAINT ejercicio_fkey FOREIGN KEY (ejercicio_id) REFERENCES Ejercicio(id),
+	CONSTRAINT usuariorutina_fkey FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
+)
+
+-- DROPS
+
+DROP TABLE Cliente
+DROP TABLE Usuario
+DROP TABLE TipoUsuario
+DROP TABLE Rutina
+DROP TABLE Ejercicio
+
+-- INSERTS
+
+INSERT INTO TipoUsuario VALUES 
+('Entrenador','A'),
+('Cliente','C')
+
+INSERT INTO Usuario VALUES 
+('Jose Luis', 'Cuevas', 'Landa','1234','EstoEsUnDirectorio','cuevas.joseluis@outlook.com',2)
+
+INSERT INTO Cliente VALUES
+(1,'03/04/1997',60.50,160)
+
+SELECT * FROM Cliente
