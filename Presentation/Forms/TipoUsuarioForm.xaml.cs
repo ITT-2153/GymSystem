e45ada48604ca8a018363328fec3e61f1a58509c
josecuevaslanda <33970466+josecuevaslanda@users.noreply.github.com>
2019-11-24 16:40:49 -0800

    Ediciones menores

--- Presentation/Forms/TipoUsuarioForm.xaml.cs
+++ Presentation/Forms/TipoUsuarioForm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Models;
+using Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +22,49 @@ namespace Presentation.Forms
     /// </summary>
     public partial class TipoUsuarioForm : UserControl
     {
+        private TipoUsuarioModel tipoUsuario = new TipoUsuarioModel();
+        bool isModifying = false;
+        string idTipoUsuario;
+        public string message;
         public TipoUsuarioForm()
         {
             InitializeComponent();
+            FillTipoUsuarioCombox();
         }
-
+        private void FillTipoUsuarioCombox()
+        {
+            TipoUsuarioCombox.Items.Add("A");
+            TipoUsuarioCombox.Items.Add("E");
+        }
+        public void SetData(string id, string nombre, string tipoUsuario)
+        {
+            isModifying = true;
+            idTipoUsuario = id;
+            TipoUsuarioCombox.SelectedItem = tipoUsuario;
+            
+        }
         private void GuardarBtn_Click(object sender, RoutedEventArgs e)
         {
+            if (isModifying == true)
+            {
+                tipoUsuario.EntityState = EntityState.Modified;
+                tipoUsuario.Id = idTipoUsuario;
+            }
+            else
+                tipoUsuario.EntityState = EntityState.Added;
 
+            tipoUsuario.Nombre = NombreTextBox.Text;
+            tipoUsuario.TipoUsuario = TipoUsuarioCombox.SelectedItem.ToString();
+
+            bool validation = new Helps.DataValidation(tipoUsuario).Validate();
+
+            if (validation == true)
+            {
+                string result = tipoUsuario.Savechanges();
+                MessageBox.Show(result);
+                //message = result;
+                //DialogResult = true;
+            }
         }
     }
 }
